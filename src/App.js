44205d7878cdb37f2ef7ff847321b31e165aa8e9
Rafael <gavinhos60@gmail.com>
2023-04-06 18:06:22 -0300

    Criacao carrinho

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import SimpleSlider from './components/carrosel';
 import Header from './components/header';
 import Whatsapp from './components/whatsapp'
-
+import ShoppingCart from './components/carrinho'
 function App() {
   return (
     <div className="App">
      <Header />
      <SimpleSlider />
      <Whatsapp />
+  
     </div>
   );
 }
